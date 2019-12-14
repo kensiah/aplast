f1ce210196c369096345ea2f381f60c57ccb7c81
kensiah <siahkianann0479@e.newera.edu.my>
2019-12-14 10:39:15 +0800

    last class

--- app/src/main/java/com/example/miscellaneous/MainActivity.java
+++ app/src/main/java/com/example/miscellaneous/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.miscellaneous;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        changeLanguage();
         setContentView(R.layout.activity_main);
+    }
+
+    private void changeLanguage(){
+        Resources res = getResources();
+        DisplayMetrics metrics = res.getDisplayMetrics();
+        android.content.res.Configuration config = res.getConfiguration();
+        config.setLocale(new Locale("zh"));
+        res.updateConfiguration(config,metrics);
     }
 }
